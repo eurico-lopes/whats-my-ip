2aea0a014a1f0c3df7883e81b6c57edfa60f02c2
Eurico <eurico.manel@gmail.com>
2023-04-17 09:09:35 +0100

    scripts to get current tag, test, build and deploy

---
+++ scripts/build-and-deploy-docker-image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+DEFAULT_TAG=$(sh ./get-tag.sh)
+TAG=${TAG:=$DEFAULT_TAG}
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
+docker build --target=production -t ${DOCKER_ID}/whats_my_ip:${TAG} -f ../Dockerfile ../.
+docker push ${DOCKER_ID}/whats_my_ip:${TAG}
---
+++ scripts/run-tests.sh
@@ -0,0 +1,2 @@
+docker build --target=test -t local-tests -f ../Dockerfile ../.
+docker run local-tests
