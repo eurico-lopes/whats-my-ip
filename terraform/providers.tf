827edd778e4957e7d04f2bc5974de491e8a2d015
Eurico <eurico.manel@gmail.com>
2023-04-18 00:27:40 +0100

    adding a provider backend in a locally deployed s3

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,17 @@
 terraform {
+  backend "s3" {
+    bucket                      = "terraform-state-bucket"
+    region                      = "us-east-1"
+    key                         = "whats-my-ip"
+    endpoint                    = "http://localhost:4566"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+    dynamodb_table              = "terraform-state-table"
+    dynamodb_endpoint           = "http://localhost:4566" #overriden to use localstack
+    iam_endpoint                = "http://localhost:4566" #overriden to use localstack
+    sts_endpoint                = "http://localhost:4566" #overriden to use localstack
+  }
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
